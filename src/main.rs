0bbf58f4175640896ac150d803699fd809962dee
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-12-16 19:33:49 +0200

    rename main to shuttle service and remove lint allow for dead code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use axum::Router;
 
 mod handlers;
-#[allow(dead_code)]
 async fn app() -> Router {
     handlers::router().await
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn shuttle_service() -> shuttle_axum::ShuttleAxum {
     dotenv::dotenv().ok();
     Ok(app().await.into())
 }
